fc52525af01f3cc0f1b048c7e334f4e9ddc283f8
zhouhannan <zhouhannan@163.com>
2019-03-08 18:41:17 +0800

    新增插入排序

--- sortFunc.js
+++ sortFunc.js
@@ -10,5 +10,25 @@ const Sort = {
       i !== minIndex && arr.swap(i, minIndex)
     }
     return arr
+  },
+  insertionSort (arr) {
+    for (let i = 1; i < arr.length; i++) {
+      // 将索引所在的值用临时变量保存，与前面的比较，比直接交换位置新能更好
+      let tmp = arr[i]
+      for (var j = i; j < arr.length; j--) {
+        if (arr[j - 1] > tmp) {
+          arr[j] = arr[j - 1]
+        } else {
+          arr[j] = tmp
+          break
+        }
+      }
+    }
+    return arr
+  },
+  mergeSort (arr) {
+    // 拆分数组，形成递归
+
+    return arr
   }
 }
